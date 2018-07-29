599287076c7431b45699a69ef7972ac513fe3fe0
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-07-29 17:11:01 +0200

    feat: add trello/diff-changes.sh

---
+++ trello/diff-changes.sh
@@ -0,0 +1,27 @@
+tree () {
+  find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
+}
+
+DIRS="$(find * -type d -maxdepth 0)"
+
+for DIR in $DIRS
+do
+  OUTPUT_FILE="$DIR.tree.txt"
+  echo "Generating $OUTPUT_FILE ..."
+  cd $DIR
+  tree >../$OUTPUT_FILE
+  cd ..
+done
+
+TWO_LAST_DIRS=($(find * -type d -maxdepth 0 | tail -2))
+DIR1="${TWO_LAST_DIRS[0]}"
+DIR2="${TWO_LAST_DIRS[1]}"
+OUTPUT_FILE="diff-from-$DIR1-to-$DIR2.log"
+
+echo "Generating diff of last two backups => $OUTPUT_FILE ..."
+# LAST_TWO_FILES="$(ls -1 *.txt | tail -2)"
+# git diff $LAST_TWO_FILES >../$OUTPUT_FILE
+
+diff -r --brief $DIR1 $DIR2 | sed -e "s/Only in $DIR2/+ /g" -e "s/Only in $DIR1/- /g" >../$OUTPUT_FILE
+
+echo "✅ Done."
