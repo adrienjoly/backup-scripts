0151042df6c785ed00f819feb46b258a0e8a935a
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-07-31 12:36:12 +0200

    add nodejs script to render a diigo outliner to an indented text file

---
+++ diigo/convert.js
@@ -0,0 +1,54 @@
+// usage:
+// node convert.js diigo-outliner-sample.json >output.txt
+
+const filename = process.argv[2] || 'diigo-outliner-sample.json';
+
+console.warn(`reading ${filename} ...`);
+const nodes = require(`./${filename}`).data.bulletpoints;
+
+console.warn('creating node index ...');
+const nodeSet = nodes.reduce((nodeSet, node) => ({
+  ...nodeSet,
+  [node.bullet_client_id]: node,
+}));
+
+console.warn('enumerating node children ...');
+const nodeChidren = nodes.reduce((nodeChidren, node) => ({
+  ...nodeChidren,
+  [node.parent_client_id]: (nodeChidren[node.parent_client_id] || []).concat(node.bullet_client_id),
+}));
+
+const byPosition = (a, b) =>
+  nodeSet[a.bullet_client_id].position - nodeSet[b.bullet_client_id].position;
+
+const recurseChildren = nodeId => ({
+  ...nodeSet[nodeId],
+  childNodes: (nodeChidren[nodeId] || [])
+    .map(recurseChildren)
+    .sort(byPosition)
+});
+
+console.warn('recreating node tree ...');
+const nodeTree = recurseChildren(0); // or log(recurseChildren)(0)
+
+const renderHTML = html => html
+  .replace(/<[^>]*>/g, '')
+  .replace(/&gt;/, '>');
+  // TODO: also replace other HTML entities
+  // TODO: create a different function that is able to interpret hyperlinks
+
+const renderTextContent = content => {
+  const text = renderHTML(content);
+  return /^\{.*\}$/.test(text) ? JSON.parse(text).title : text;
+  // TODO: create a different function that is able to also extract the `url` field of the JSON
+};
+
+const indent = (text, depth = 0) => `${''.padStart(depth * 2)}${text}`;
+const renderIndentedNode = (node, depth = 0) => 
+  [ indent(`- ${renderTextContent(node.content)}`, depth) ]
+    .concat(node.childNodes.map(node => renderIndentedNode(node, depth + 1)))
+    .join('\n');
+
+console.log(renderIndentedNode(nodeTree.childNodes[0]));
+// TODO also render to YAML objects (incl. urls of links and basic text formatting)
+// TODO also render to HTML (incl. urls of links and basic text formatting)
