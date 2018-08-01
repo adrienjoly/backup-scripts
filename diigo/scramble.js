2f4df39378c17b130ce7c2c7a15e0d7eee8d2c47
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-08-01 11:44:00 +0200

    refacto and fix scrambler to not scramble HTML elements representing a json link

--- diigo/scramble.js
+++ diigo/scramble.js
@@ -14,15 +14,47 @@ const makeCharacterPicker = characters => {
   };
 }
 
-const pickLetter = () => 'a'; // makeCharacterPicker('abcdefghijklmnopqrstuvwxyz'); // TODO: re-enable
-const pickNumber = makeCharacterPicker('0123456789');
+const makeLetterPicker = () => () => 'a'; // makeCharacterPicker('abcdefghijklmnopqrstuvwxyz'); // TODO: re-enable
+const makeNumberPicker = () => makeCharacterPicker('0123456789');
 
-const scrambleText = text => text
-  // .replace(/[0-9]/g, pickNumber) // TODO: re-enable
-  .replace(/[a-z]/g, pickLetter)
-  .replace(/[A-Z]/g, () => pickLetter().toUpperCase())
+const makeScrambler = () => {
+  const pickLetter = makeLetterPicker();
+  const pickNumber = makeNumberPicker();
+  
+  const scrambleText = text => text
+    // .replace(/[0-9]/g, pickNumber) // TODO: re-enable
+    .replace(/[a-z]/g, pickLetter)
+    .replace(/[A-Z]/g, () => pickLetter().toUpperCase());
 
-const scrambleContent = scrambleText; // TODO: leave HTML elements and JSON fields as is
+  return scrambleText;
+};
+
+const scrambleText = makeScrambler();
+
+const RE_JSON_LINK = /<span class=\"diigoItemFlag\">(\{[^\}]*\})<\/span>/g;
+const RE_LINK = /<span class=\"diigoItemFlag\">\{[^\}]*\}<\/span>/g;
+
+const scrambleContent = content => {
+  const plainTexts = content.split(RE_LINK).map(scrambleText);
+  let jsonLinks = [];
+  let lastMatch;
+  while (lastMatch = RE_JSON_LINK.exec(content)) {
+    const link = JSON.parse(lastMatch[1]);
+    // const scrambleText = makeScrambler(); // make a separate scrambler, in order to avoid side effects to scrambling of plain-text content
+    jsonLinks.push({
+      ...link,
+      title: scrambleText(link.title),
+      url: link.url ? scrambleText(link.url) : undefined,
+    });
+  }
+  const renderJsonLink = json => !json ? '' : `<span class="diigoItemFlag">${JSON.stringify(json)}</span>`;
+  const scrambled = plainTexts
+    .map(text => text + renderJsonLink(jsonLinks.shift()))
+    .join('');
+  // console.warn('=>', scrambled);
+  return scrambled;
+  // TODO: also leave HTML elements as is
+};
 
 const scrambleNode = node => ({
   ...node,
@@ -37,3 +69,7 @@ const scrambled = {
 };
 
 console.log(JSON.stringify(scrambled, null, 2))
+
+// const twoLinks = '[side-project] Player whyd local ? &lt;-- <span class=\"diigoItemFlag\">{\"title\":\"1.\",\"url\":\"http://electron.atom.io\"}</span><span></span> coucou [side-project] Player whyd local ? &lt;-- <span class=\"diigoItemFlag\">{\"title\":\"2.\",\"url\":\"http://electron.atom.io\"}</span><span></span>';
+
+// console.log(scrambleContent(twoLinks));
