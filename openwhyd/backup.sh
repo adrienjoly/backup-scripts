ca482e2830f3c92e3cc1edfb1c0553133ff3bc38
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-12-19 20:47:19 +0100

    feat: add backup script for openwhyd.org

---
+++ openwhyd/backup.sh
@@ -0,0 +1,19 @@
+set -e # this script will exit if any command returns a non-null value
+
+TIMESTAMP="$(date +'%Y-%m-%d_%H-%M')_backup"
+BACKUP_PATH="${PWD}/${TIMESTAMP}"
+
+# ℹ️ the script will generate a subdirectory in it like: 2018-07-29_12-13-18_backup
+mkdir -p $BACKUP_PATH
+
+source ./.env # loads PROFILE_URL
+
+echo "Backing up Openwhyd playlists from ${PROFILE_URL} to ${BACKUP_PATH} ..."
+
+docker build -t "openwhyd-pl-dl" https://github.com/adrienjoly/openwhyd-pl-dl.git
+docker run -it --rm \
+           -v "${BACKUP_PATH}:/app" -w "/app" "openwhyd-pl-dl" \
+           "/bin/bash" -c "/bin/bash <(wget -qO- https://raw.githubusercontent.com/adrienjoly/openwhyd-pl-dl/master/openwhyd-pl-dl-json.sh) ${PROFILE_URL}"
+
+echo "✅ Done."
+echo "ℹ️ Next steps: run ./diff-changes.sh && zip-and-upload.sh && ./clean-up.sh"
