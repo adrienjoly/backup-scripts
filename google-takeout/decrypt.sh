28be63f5683894060ceb25549241fd5178019233
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2022-07-14 15:34:00 +0200

    google-takeout: clean-up scripts

--- google-takeout/decrypt.sh
+++ google-takeout/decrypt.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
+source .env # must define BITWARDEN_ENTRY_ID, cf $ bw list items --search "Personal data backup"
+
 echo "🔒 getting gpg password from bitwarden"
-BITWARDEN_ENTRY_ID="cbaf30f6-c20d-4c37-b52b-abb20117bba5" #  cf $ bw list items --search "Personal data backup"
 GPG_PASSPHRASE="$(bw get password ${BITWARDEN_ENTRY_ID})"
 
-(echo "${GPG_PASSPHRASE}" | gpg --batch --yes --passphrase-fd 0 --decrypt "$1") > "$1.decrypted"
+INPUT_FILE="$1"
+DEST_FILE="${INPUT_FILE}.decrypted"
+
+(echo "${GPG_PASSPHRASE}" | gpg --batch --yes --passphrase-fd 0 --decrypt "${INPUT_FILE}") > "${DEST_FILE}"
+
+echo "✅ Done: ${DEST_FILE}"
--- google-takeout/encrypt.sh
+++ google-takeout/encrypt.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
+source .env # must define BITWARDEN_ENTRY_ID, cf $ bw list items --search "Personal data backup"
+
 echo "🔒 getting gpg password from bitwarden"
-BITWARDEN_ENTRY_ID="cbaf30f6-c20d-4c37-b52b-abb20117bba5" #  cf $ bw list items --search "Personal data backup"
 GPG_PASSPHRASE="$(bw get password ${BITWARDEN_ENTRY_ID})"
 
 INPUT_FILE="$1"
 DEST_FILE="${INPUT_FILE}.gpg"
 
-# Encrypt
 echo "${GPG_PASSPHRASE}" | gpg --batch --yes --passphrase-fd 0 -o "${DEST_FILE}" --symmetric "${INPUT_FILE}"
 
-# Decrypt
-# echo "${GPG_PASSPHRASE}" | gpg --batch --yes --passphrase-fd 0 --decrypt "${DEST_FILE}" > "${DECRYPTED_FILE}"
+echo "✅ Done."
+echo "ℹ️ Next steps: upload ${DEST_FILE} to a safe place"
