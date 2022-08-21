d62cac13261ad9447e2f345e2a8dc64ab8f4ea04
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2022-08-21 11:36:08 +0200

    clean-up 1

--- hackmd/src/backup-hackmd.js
+++ hackmd/src/backup-hackmd.js
@@ -1,7 +1,6 @@
-const fs = require("fs");
-const https = require("https");
-const { BlockList } = require("net");
 const puppeteer = require("puppeteer");
+
+const DEBUG = false
 
 const [, , HACKMD_EMAIL, HACKMD_PWD] = process.argv;
 
@@ -13,91 +12,30 @@ const [, , HACKMD_EMAIL, HACKMD_PWD] = process.argv;
   await page.goto("https://hackmd.io/login", { waitUntil: "networkidle2" });
   await page.type("input[type=email]", HACKMD_EMAIL);
   await page.type("input[type=password]", HACKMD_PWD);
-  //  await page.screenshot({path: 'example.png'});
+  if (DEBUG) await page.screenshot({path: 'login-scren.png'});
   await page.click("input[type=submit]");
-  //   await page.waitForTimeout(5000); // wait for 5 seconds
-  const frame = await new Promise((resolve) =>
+
+  await new Promise((resolve) =>
     page.on("framenavigated", (frame) => {
       const url = frame.url();
       console.warn(`navigated to ${url}`);
       if (url === "https://hackmd.io/?nav=overview") resolve(frame);
     })
   );
-  // await page.screenshot({path: 'example2.png'});
-  // await page.goto('https://hackmd.io/settings#note', { waitUntil: 'networkidle2' });
-  // await page.screenshot({ path: "example3.png" });
-
-  let fn = async (uri) => {
-    const res = await fetch(uri, { credentials: "same-origin" });
-    const blob = await res.blob();
-    // const bufferArray = await blob.arrayBuffer();
-    return {
-      size: blob.size,
-      text: await blob.text(),
-      // base64String: btoa([].reduce.call(new Uint8Array(bufferArray),function(p,c){return p+String.fromCharCode(c)},'')),
-    };
-  };
-
-  // await page.goto('https://hackmd.io/exportAllNotes', { waitUntil: 'networkidle2' });
-  // const { size, text, base64String } = await page.evaluate(fn, 'https://hackmd.io/exportAllNotes')
-
-  browser.on("targetcreated", async (target) => {
-    let s = target.url();
-    console.warn("targetcreated", s);
-    // //the test opens an about:blank to start - ignore this
-    // if (s == 'about:blank') {
-    //     return;
-    // }
-    // //unencode the characters after removing the content type
-    // s = s.replace("data:text/csv;charset=utf-8,", "");
-    // //clean up string by unencoding the %xx
-    // ...
-    // fs.writeFile("/tmp/download.csv", s, function(err) {
-    //     if(err) {
-    //         console.log(err);
-    //         return;
-    //     }
-    //     console.log("The file was saved!");
-    // });
-  });
-
-  // page.on('response', async function pageOnResponseRequest(response) {
-  //   // if (response.frame() === page.mainFrame() && response.request().isNavigationRequest()) {
-  //     const statusCode = response.status();
-  //     const headers = response.headers();
-  //     console.warn('onresponse', { url: response.url(), isNavigReq: response.request().isNavigationRequest(), statusCode, headers})
-
-  //     if (response.url() !== "https://hackmd.io/exportAllNotes") return;
-
-  //     try {
-  //       // console.warn("response", await response.text())
-  //       fs.writeFileSync('coucou.zip', await response.buffer())
-  //       // At this point you have access to status code and headers which you can use to detect that it's an html document, an image, a downloadable document, etc...
-  //       console.warn("done saveing coucou.zip")
-  //     } catch (err) {
-  //       console.warn(err)
-  //     }
-  //   // }
-  // });
 
   console.warn("waiting...");
   await page.waitForTimeout(5000); // wait for 5 seconds
 
   await page.setRequestInterception(true);
 
-  /*const response = await*/ page
+  page
     .goto("https://hackmd.io/exportAllNotes")
-    .catch((e) => console.warn("skipped error", e.message));
-  // await fs.writeFileSync('coucou.zip', await response.buffer())
+    .catch((e) => console.warn("(i) ignored error:", e.message));
 
   const xRequest = await new Promise((resolve) => {
     page.on("request", (interceptedRequest) => {
-      console.warn(
-        "intercepted",
-        interceptedRequest.method(),
-        interceptedRequest.url()
-      );
-      interceptedRequest.abort(); //stop intercepting requests
+      console.warn(`intercepted ${interceptedRequest.method()} ${interceptedRequest.url()}`)
+      interceptedRequest.abort();
       resolve(interceptedRequest);
     });
   });
