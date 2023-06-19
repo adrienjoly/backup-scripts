2af3845864fb4ffbe3fa857a11bda97d61575591
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-06-19 13:39:59 +0200

    fix(trello): support target directory that contains spaces

--- trello/zip-and-upload.sh
+++ trello/zip-and-upload.sh
@@ -5,14 +5,14 @@ source ../.env # must define UPLOAD_PATH
 LAST_BACKUP_DIR="$(find * -type d -maxdepth 0 | tail -1)"
 
 echo "Archiving last backup files to $LAST_BACKUP_DIR.zip ..."
-zip -q -r -9 $LAST_BACKUP_DIR.zip $LAST_BACKUP_DIR
+zip -q -r -9 "${LAST_BACKUP_DIR}.zip" "$LAST_BACKUP_DIR"
 
-echo "Moving archive to $UPLOAD_PATH/trello ..."
-mkdir -p $UPLOAD_PATH/trello
-mv $LAST_BACKUP_DIR.* $UPLOAD_PATH/trello
+echo "Moving archive to ${UPLOAD_PATH}/trello ..."
+mkdir -p "${UPLOAD_PATH}/trello"
+mv "${LAST_BACKUP_DIR}.*" "${UPLOAD_PATH}/trello"
 
-echo "Renaming $LAST_BACKUP_DIR to ${LAST_BACKUP_DIR}_archived ..."
-mv $LAST_BACKUP_DIR "${LAST_BACKUP_DIR}_archived"
+echo "Renaming ${LAST_BACKUP_DIR} to ${LAST_BACKUP_DIR}_archived ..."
+mv "${LAST_BACKUP_DIR}" "${LAST_BACKUP_DIR}_archived"
 
 echo "✅ Done."
 echo "ℹ️ Next steps: run ./clean-up.sh"
