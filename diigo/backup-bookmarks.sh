b2996922643e6cf58ec878c3f068fd1ee46ea3f5
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-06-19 10:36:27 +0200

    fix(diigo): write separate files + share pre-requisites

--- diigo/backup-bookmarks.sh
+++ diigo/backup-bookmarks.sh
@@ -1,5 +1,7 @@
 set -e # this script will exit if any command returns a non-null value
 
+# pre-requisite: $ brew install jq
+
 source .env # must define DIIGO_USERNAME and DIIGO_URL_ENCODED_PASSWORD
 
 BACKUP_PATH="$PWD"
@@ -10,11 +12,11 @@ echo "Backing up Diigo bookmarks to $OUTPUT_FILE ..."
 
 # Status: pagination is not working as expected... maybe because of jq?
 
-curl "https://$DIIGO_USERNAME:$DIIGO_URL_ENCODED_PASSWORD@secure.diigo.com/api/v2/bookmarks?user=$DIIGO_USERNAME&filter=all&sort=0&count=10&start=0" | jq . >1.json
+curl "https://$DIIGO_USERNAME:$DIIGO_URL_ENCODED_PASSWORD@secure.diigo.com/api/v2/bookmarks?user=$DIIGO_USERNAME&filter=all&sort=0&count=10&start=0" | jq . >0.json
 
 curl "https://$DIIGO_USERNAME:$DIIGO_URL_ENCODED_PASSWORD@secure.diigo.com/api/v2/bookmarks?user=$DIIGO_USERNAME&filter=all&sort=0&count=10&start=1" | jq . >1.json
 
-curl "https://$DIIGO_USERNAME:$DIIGO_URL_ENCODED_PASSWORD@secure.diigo.com/api/v2/bookmarks?user=$DIIGO_USERNAME&filter=all&sort=0&count=10&start=2" | jq . >1.json
+curl "https://$DIIGO_USERNAME:$DIIGO_URL_ENCODED_PASSWORD@secure.diigo.com/api/v2/bookmarks?user=$DIIGO_USERNAME&filter=all&sort=0&count=10&start=2" | jq . >2.json
 
 # echo "✅ Done."
 # echo "ℹ️ Next steps: run ./diff-changes.sh && zip-and-upload.sh && ./clean-up.sh"
