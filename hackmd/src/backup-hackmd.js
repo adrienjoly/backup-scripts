1bee53f84f47fd780bff75f8f24a36b7c9c0da78
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-06-19 10:17:12 +0200

    debug(hackmd): it seems stuck because of a recaptcha

--- hackmd/src/backup-hackmd.js
+++ hackmd/src/backup-hackmd.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer");
 
-const DEBUG = false;
+const { DEBUG } = process.env; // for verbose logs, pass DEBUG='*'
 
 const [, , HACKMD_EMAIL, HACKMD_PWD] = process.argv;
 
@@ -28,10 +28,13 @@ const interceptRequests = async (page) => {
   const page = await browser.newPage();
 
   // login
+  if (DEBUG) console.warn(`connecting to hackmd.io...`);
   await page.goto("https://hackmd.io/login", { waitUntil: "networkidle2" });
+  if (DEBUG) console.warn(`logging in to hackmd.io...`);
   await page.type("input[type=email]", HACKMD_EMAIL);
   await page.type("input[type=password]", HACKMD_PWD);
   if (DEBUG) await page.screenshot({ path: "login-scren.png" });
+  if (DEBUG) console.warn(`submitting credentials...`);
   const intercept = await interceptRequests(page);
   await page.click("input[type=submit]");
   const interceptedRequest = await intercept.untilRequest(
@@ -39,6 +42,7 @@ const interceptRequests = async (page) => {
   );
 
   // request the backup
+  if (DEBUG) console.warn(`requesting notes...`);
   const cookies = await page.cookies();
   const response = await fetch("https://hackmd.io/exportAllNotes", {
     encoding: null,
@@ -49,6 +53,7 @@ const interceptRequests = async (page) => {
   });
 
   // download the backup
+  if (DEBUG) console.warn(`downloading notes...`);
   const blob = await response.blob();
   console.warn(`downloading ${blob.size} bytes, type: ${blob.type}...`);
   process.stdout.write(Buffer.from(await blob.arrayBuffer()));
