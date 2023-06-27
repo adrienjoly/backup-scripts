cfdbe7b1065d3f156604564d24b9f6527163bc95
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-06-27 15:20:57 +0200

    docs(google-takeout): how to install+use `bw` CLI

--- google-takeout/decrypt.sh
+++ google-takeout/decrypt.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-source .env # must define BITWARDEN_ENTRY_ID, cf $ bw list items --search "Personal data backup"
+source .env # must define BITWARDEN_ENTRY_ID, cf $ brew install bitwarden-cli && bw list items --search "Personal data backup" | jq '.[0].id'
 
-echo "🔒 getting gpg password from bitwarden"
+echo "🔒 getting gpg password from bitwarden..."
 GPG_PASSPHRASE="$(bw get password ${BITWARDEN_ENTRY_ID})"
 
 INPUT_FILE="$1"
 DEST_FILE="${INPUT_FILE}.decrypted"
 
+echo "🐌 decrypting to ${DEST_FILE}..."
 (echo "${GPG_PASSPHRASE}" | gpg --batch --yes --passphrase-fd 0 --decrypt "${INPUT_FILE}") > "${DEST_FILE}"
 
 echo "✅ Done: ${DEST_FILE}"
--- google-takeout/encrypt.sh
+++ google-takeout/encrypt.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-source .env # must define BITWARDEN_ENTRY_ID, cf $ bw list items --search "Personal data backup"
+source .env # must define BITWARDEN_ENTRY_ID, cf $ brew install bitwarden-cli && bw list items --search "Personal data backup" | jq '.[0].id'
 
-echo "🔒 getting gpg password from bitwarden"
+echo "🔒 getting gpg password from bitwarden..."
 GPG_PASSPHRASE="$(bw get password ${BITWARDEN_ENTRY_ID})"
 
 INPUT_FILE="$1"
 DEST_FILE="${INPUT_FILE}.gpg"
 
+echo "🐌 encrypting to ${DEST_FILE}..."
 echo "${GPG_PASSPHRASE}" | gpg --batch --yes --passphrase-fd 0 -o "${DEST_FILE}" --symmetric "${INPUT_FILE}"
 
 echo "✅ Done."
