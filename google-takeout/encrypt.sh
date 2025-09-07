27f150321fe00ac10398136e3196c60b54e1a95f
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2025-09-07 10:28:28 +0200

    feat(google-takeout): support multiple input files at once

--- google-takeout/encrypt.sh
+++ google-takeout/encrypt.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 set -e
 
+# Encrypts files passed as arguments
+# E.g. $ ./encrypt.sh "~/Downloads/google takeout/"*
+
 source .env # must define BITWARDEN_ENTRY_ID, cf $ brew install bitwarden-cli && bw list items --search "Personal data backup" | jq '.[0].id'
 
 echo "🔒 getting gpg password from bitwarden..."
 GPG_PASSPHRASE="$(bw get password ${BITWARDEN_ENTRY_ID})"
 
-INPUT_FILE="$1"
-DEST_FILE="${INPUT_FILE}.gpg"
+for INPUT_FILE in "$@"; do
+	DEST_FILE="${INPUT_FILE}.gpg"
+	echo "🐌 encrypting to ${DEST_FILE}..."
+	echo "${GPG_PASSPHRASE}" | gpg --batch --yes --passphrase-fd 0 -o "${DEST_FILE}" --symmetric "${INPUT_FILE}"
+	echo "✅ Done encrypting ${INPUT_FILE}."
+done
 
-echo "🐌 encrypting to ${DEST_FILE}..."
-echo "${GPG_PASSPHRASE}" | gpg --batch --yes --passphrase-fd 0 -o "${DEST_FILE}" --symmetric "${INPUT_FILE}"
-
-echo "✅ Done."
-echo "ℹ️ Next steps: upload ${DEST_FILE} to a safe place"
+echo "ℹ️ Next steps: upload crypted files to a safe place"
